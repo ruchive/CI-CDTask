cc441f6297b56fc2fb9b93bffabeabfb99bcc2eb
Ruchive <Ruchi.Verma@amdocs.com>
2019-03-15 19:20:56 +0530

    change

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -9,7 +9,7 @@
 </head>
 <body>
 	<h1>Hello JSP and Servlet!</h1>
-	<form action="Holla!!" method="post">
+	<form action="HelloServlet" method="post">
 		Enter your name: <input type="text" name="yourName" size="20">
 		<input type="submit" value="Call Servlet" />
 	</form>
--- src/main/java/com/qaagility/javaee/HelloServlet.java
+++ src/main/java/com/qaagility/javaee/HelloServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/helloServlet")
+@WebServlet("/Holla!!")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
